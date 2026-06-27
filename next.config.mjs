c6866f40f70eee3859caac210feee65331a35711
Mohammad Almashahra <mialmashahreh22@cit.just.edu.jo>
2026-06-27 15:22:49 +0300

    Deploy SOCFlow with GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/socflow-automation-portfolio",
+  trailingSlash: true,
 };
 
 export default nextConfig;
